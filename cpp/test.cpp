394c1bd3ecd8f358f4325b43962b4624fd9074b1
Simon Leigh <simon.leigh13@imperial.ac.uk>
2014-11-20 16:03:21 +0000

    Working test adding to DB

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -11,13 +11,16 @@ int main(){
   PowerDataConnector pd = PowerDataConnector("tcp://192.168.1.113:5556");
   PDDBConnector dbConn = PDDBConnector("localhost", "logger", "l0gg3r");
 
-  // Connect to server
+  // Connect to zmq server
   try {
     pd.connect();
   }
   catch(zmqpp::exception &e){
     std::cout << "Exception thrown: " << e.what() << std::endl;
   }
+
+  // Connect to DB
+  dbConn.connectAndSetDb("kinetica_power_data");
     
   
   std::string text;
@@ -33,6 +36,10 @@ int main(){
     // Receive text to vector of doubles method
     std::cout << "Testing receiveToVector() method: " << std::endl;
     std::vector<double> dVector = pd.receiveToVector();
+
+    // Try and add to database
+    std::cout << "Testing db.sendUpdate(Vector) method: " << std::endl;
+    dbConn.sendUpdate(dVector);
     
     // Have to iterate the vector to print it in c++
     for (std::vector<double>::const_iterator i = dVector.begin(); i != dVector.end(); ++i){
